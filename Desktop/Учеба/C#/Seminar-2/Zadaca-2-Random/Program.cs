c485752abf467b94c52c5588f16b6db72eee2afe
Iurii Fastovskii <yurafm@gmail.com>
2022-04-16 11:07:47 +0300

    2

---
+++ Desktop/Учеба/C#/Seminar-2/Zadaca-2-Random/Program.cs
@@ -0,0 +1,10 @@
+﻿// Случайное число
+int a = new Random().Next(10,99);
+int b = a / 10;
+int c = a%10;
+int max = b;
+if (c > b ) max = c;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine($"Максимальное число {max}");
+
