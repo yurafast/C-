13a0bb6ee979d35789ce122faf39dd980fbf2232
Iurii Fastovskii <yurafm@gmail.com>
2022-04-16 11:23:46 +0300

    11

---
+++ Desktop/Учеба/C#/Seminar-2/Zadaca-3-Kratnoe/Program.cs
@@ -0,0 +1,14 @@
+﻿// Кратное число
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (b % a == 0) 
+{
+Console.WriteLine($"Число {b} КРАТНО {a}");
+int c = b / a;
+Console.WriteLine(c);
+}
+else
+{
+Console.WriteLine($"Число {b} НЕ кратно {a}");
+}
+
