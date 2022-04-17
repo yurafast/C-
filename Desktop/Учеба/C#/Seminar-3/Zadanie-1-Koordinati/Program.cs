1dcfb5c42ee31beecf2e3c8c59bc1106c6f05e93
Iurii Fastovskii <yurafm@gmail.com>
2022-04-17 10:26:23 +0300

    1

---
+++ Desktop/Учеба/C#/Seminar-3/Zadanie-1-Koordinati/Program.cs
@@ -0,0 +1,25 @@
+﻿// Координаты
+double X = new Random().Next(-100 , 100);
+double Y = new Random().Next(-100 , 100);
+Console.WriteLine(X);
+Console.WriteLine(Y);
+if(X < 0 & Y < 0)
+{
+    Console.WriteLine("Точка находится в 3й четверти");
+} 
+if(X > 0 & Y > 0)
+{
+    Console.WriteLine("Точка находится в 1й четверти");
+} 
+if(X < 0 & Y > 0)
+{
+    Console.WriteLine("Точка находится в 2й четверти");
+} 
+if(X > 0 & Y < 0)
+{
+    Console.WriteLine("Точка находится в 4й четверти");
+} 
+if (X == 0 & Y == 0)
+{
+    Console.WriteLine("Точка НЕ в четверти");
+} 
