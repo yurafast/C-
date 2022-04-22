b6cf550cff165450f9f4561d57422e2bec102ab2
Iurii Fastovskii <yurafm@gmail.com>
2022-04-22 10:27:02 +0300

    zadaca-4

---
+++ Desktop/Учеба/C#/Seminar-3/Zadanie-2-Rasstoianie_2D/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вводим координаты 2х точек и находим расстояние между точками
+Console.Clear();
+Console.WriteLine("Введите координаты 1й точки");
+Console.Write("X1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Y1: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("X2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Y2: ");
+int y2 = int.Parse(Console.ReadLine());
+double lenght = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine($"Расстояние между точками равно {lenght}");
