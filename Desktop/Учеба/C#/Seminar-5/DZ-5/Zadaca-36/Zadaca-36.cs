6a8fa79169883df406867130c996f4dfb9dcd563
Iurii Fastovskii <yurafm@gmail.com>
2022-04-30 02:48:56 +0300

    DZ-5

---
+++ Desktop/Учеба/C#/Seminar-5/DZ-5/Zadaca-36/Zadaca-36.cs
@@ -0,0 +1,35 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный
+//случайными числами. Найдите сумму элементов, стоящих
+//на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] mas = new int[10];
+
+void Printo(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = new Random().Next(1, 100);
+}
+
+Printo(mas);
+
+int sum=0;
+
+for(int i=0; i< mas.Length; i++)
+{
+    if(mas[i]%2==0)
+    {
+        sum=sum+mas[i];
+    }
+}
+Console.WriteLine();
+
+Console.WriteLine($"Сумма четных эллементов массива равна: {sum}");
