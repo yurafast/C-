e41c3174e8fc1f42456f6cd2382faf17fd49e3b8
Iurii Fastovskii <yurafm@gmail.com>
2022-05-06 17:47:17 +0300

    DZ-6-upp1

--- Desktop/Учеба/C#/Seminar-6/DZ-6/DZ-ALL.cs
+++ Desktop/Учеба/C#/Seminar-6/DZ-6/DZ-ALL.cs
@@ -40,8 +40,20 @@ Console.Write("Введите число b2: ");
 double b2 = double.Parse(Console.ReadLine());
 Console.Write("Введите число k2: ");
 double k2 = double.Parse(Console.ReadLine());
-double x = (b2 - b1) / (k1 - k2);
-double y = k1 *x + b1;
-// int y = k1 * x + b1;
-// int y1 = k2 * x + b2;
-Console.WriteLine($"точка пересечения прямых ({x} ; {y})");
+Console.WriteLine();
+
+if ((k1 / k2) == (b1 / b2))
+{
+    Console.WriteLine("Прямые совпадают");
+}
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"точка пересечения прямых ({x} ; {y})");
+}
