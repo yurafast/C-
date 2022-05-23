32d3ee1a7db5d87b7cd2559f6cde27871110379c
Yurafast <yurafm@gmail.com>
2022-05-23 08:41:43 +0300

    55

---
+++ Seminar-8-Двумерные массивы/DZ-8/Zadaca-54/Zadaca-54.cs
@@ -0,0 +1,45 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+int m = 3, n = 3;
+
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+
+void Print(int[,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            Console.Write(mass[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Print(array);
+Console.WriteLine();
+int temp;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int k = j + 1; k < array.GetLength(1); k++)
+        {
+            if (array[i, j] < array[i, k])
+            {
+                temp = array[i, k];
+                array[i, k] = array[i, j];
+                array[i, j] = temp;
+            }
+        }
+    }
+}
+
+
+Print(array);
