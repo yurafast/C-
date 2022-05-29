c3da7e7a2fa74bc5518a7577b05af06985622880
Iurii Fastovskii <102867873+yurafast@users.noreply.github.com>
2022-05-29 18:11:54 +0300

    Update DZ-9.cs

--- Desktop/Учеба/C#/DZ-9.cs
+++ Desktop/Учеба/C#/DZ-9.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от M до N.
+// M = 6; N = 2. -> "6, 5, 4, 3, 2"
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7, 8"
+
+void Func(int c, int d)
+{
+    if (c < d)
+        return;
+    Console.Write(c + " ");
+    c = c - 1;
+    Func(c, d);
+}
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n : ");
+int n = int.Parse(Console.ReadLine());
+Func(m, n);
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Func(int c, int d)
+{
+    if (c == d)
+        return c;
+
+    return d + Func(c, d - 1);
+}
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n : ");
+int n = int.Parse(Console.ReadLine());
+int sum = Func(m, n);
+Console.WriteLine(sum);
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью
+// рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+int A(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+        if ((m > 0) && (n == 0))
+        return A(m - 1, 1);
+            else
+                return A(m - 1, A(m, n - 1));
+    
+}
+int result = A(2, 2);
+Console.WriteLine(result);
